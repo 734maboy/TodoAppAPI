0649d6d62518caa2bf957c7a03b973d129f227ff
734maboy <supamem74@gmail.com>
2021-12-13 20:48:20 +0300

    new: Migration initiated

--- TodoAppAPI/AppContext.cs
+++ TodoAppAPI/AppContext.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TodoAppAPI.Models;
 
 namespace TodoAppAPI
 {
 	public class AppContext : DbContext
 	{
+
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<TodoItem> TodoItems { get; set; }
+		public DbSet<Status> Statuses { get; set; }
+		public DbSet<User> Users { get; set; }
 		public AppContext() {
 			Database.EnsureCreated();
 		}
-
+		/// <summary>
+		/// Настройка подключения к БД
+		/// </summary>
+		/// <param name="optionsBuilder"></param>
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoBase;Trusted_Connection=True;");
 		}
---
+++ TodoAppAPI/Migrations/20211213174426_Initial.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoAppAPI.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 40, nullable: false),
+                    Login = table.Column<string>(maxLength: 20, nullable: false),
+                    Password = table.Column<string>(maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    StatusId = table.Column<Guid>(nullable: false),
+                    CategoryId = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    FinishDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TodoItem_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TodoItem_Status_StatusId",
+                        column: x => x.StatusId,
+                        principalTable: "Status",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TodoItem_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItem_CategoryId",
+                table: "TodoItem",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItem_StatusId",
+                table: "TodoItem",
+                column: "StatusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TodoItem_UserId",
+                table: "TodoItem",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
