45b485ff6435ab6d247787ce08e7062a08ad0d71
734maboy <supamem74@gmail.com>
2021-12-13 21:03:16 +0300

    fix: Changed property 'name' => 'Name'

--- TodoAppAPI/Models/Category.cs
+++ TodoAppAPI/Models/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace TodoAppAPI.Models {
 	public class Category : BaseModel {
 		[Required, MaxLength(30), MinLength(1)]
-		public string name { get; set; }
+		public string Name { get; set; }
 		public List<TodoItem> TodoItems { get; set; }
 	}
 }
