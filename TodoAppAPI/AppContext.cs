4fbd093b6fabbbe8e623572dbe22134560dc29b3
734maboy <supamem74@gmail.com>
2021-12-13 19:51:50 +0300

    new: AppContext created

---
+++ TodoAppAPI/AppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoAppAPI
+{
+	public class AppContext : DbContext
+	{
+		public AppContext() {
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TodoBase;Trusted_Connection=True;");
+		}
+
+
+	}
+}
